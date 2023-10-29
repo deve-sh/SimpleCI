33718003df6bae63c0b1e0610203c5d6258af8ed
Devesh Kumar <devesh2027@gmail.com>
2023-10-29 16:01:36 +0530

    feat: runner: Add status to individual steps

--- runner/lib/run-info.js
+++ runner/lib/run-info.js
@@ -8,9 +8,20 @@ class RunInfo {
 	 */
 	status = "in-progress";
 	/**
-	 * @type {{stepName:string, id: string, from: string; to: string; logPool: { type: typeof this.status, log: string, ts: string; }[] }[]}
+	 * @type {{
+	 * 	stepName:string;
+	 * 	id: string;
+	 * 	from: string;
+	 * 	to: string;
+	 * 	status: typeof this.status;
+	 * 	logPool: {
+	 * 		type: 'error' | 'info',
+	 * 		log: string,
+	 * 		ts: string;
+	 * 	}[]
+	 * }[]}
 	 */
-	stepsInfo = [];
+	stepsOutcome = [];
 	/**
 	 * @type {Record<string, string>}
 	 */
@@ -24,37 +35,44 @@ class RunInfo {
 
 	markNewStep = (
 		/**
-		 * @type { Partial<typeof this.stepsInfo[number]> }
+		 * @type { Partial<typeof this.stepsOutcome[number]> }
 		 */
 		step
 	) => {
-		this.stepsInfo.push({
+		this.stepsOutcome.push({
 			stepName: step.stepName || "",
 			id: uuidv4(),
 			from: new Date().toISOString(),
 			logPool: [],
 			to: "",
+			status: "in-progress",
 		});
 	};
 
-	markStepEnd = () => {
-		const currentStep = this.stepsInfo[this.stepsInfo.length - 1];
+	/**
+	 * @type {(status?:'errored' | 'finished') => void}
+	 */
+	markStepEnd = (status) => {
+		const currentStep = this.stepsOutcome[this.stepsOutcome.length - 1];
 		if (!currentStep) return;
 
-		this.stepsInfo[this.stepsInfo.length - 1].to = new Date().toISOString();
+		this.stepsOutcome[this.stepsOutcome.length - 1].status =
+			status || "finished";
+		this.stepsOutcome[this.stepsOutcome.length - 1].to =
+			new Date().toISOString();
 	};
 
 	addLogToCurrentStep = (
 		/**
-		 * @type { Partial<typeof this.stepsInfo[number]['logPool'][number]> }
+		 * @type { Partial<typeof this.stepsOutcome[number]['logPool'][number]> }
 		 */
 		log
 	) => {
-		const currentStep = this.stepsInfo[this.stepsInfo.length - 1];
+		const currentStep = this.stepsOutcome[this.stepsOutcome.length - 1];
 		if (!currentStep) return;
 
 		// @ts-expect-error
-		this.stepsInfo[this.stepsInfo.length - 1].logPool.push(log);
+		this.stepsOutcome[this.stepsOutcome.length - 1].logPool.push(log);
 
 		// TODO: Also send this log to a realtime database or listener for the user to see an append-only log of current job.
 	};
