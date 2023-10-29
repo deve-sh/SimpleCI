d03c9a8dc69fbdc60a5a88b2d27460785c47b8b1
Devesh Kumar <devesh2027@gmail.com>
2023-10-29 23:12:02 +0530

    runner: Update onCreate to onUpdate

--- runner/index.js
+++ runner/index.js
@@ -2,6 +2,7 @@ const functions = require("firebase-functions");
 
 const runnerSettings = {
 	standard: {
+		region: "asia-south1",
 		timeoutSeconds: 150,
 		memory: "512MB",
 	},
@@ -16,59 +17,70 @@ const runnerSettings = {
 };
 exports.pipelineRunnerStandard = functions
 	.runWith(runnerSettings.standard)
+	.region("asia-south1")
 	.firestore.document("ci-job-runs-queue/standard")
-	.onCreate((snap) => {
+	.onWrite((change) => {
+		if (!change.after.exists) return;
 		const pipelineRunnerController = require("./api-controllers/pipeline-runner-controller");
-		return pipelineRunnerController(snap);
+		return pipelineRunnerController(change.after);
 	});
 
 exports.pipelineRunnerMedium = functions
 	.runWith(runnerSettings.medium)
+	.region("asia-south1")
 	.firestore.document("ci-job-runs-queue/medium")
-	.onCreate((snap) => {
+	.onWrite((change) => {
+		if (!change.after.exists) return;
 		const pipelineRunnerController = require("./api-controllers/pipeline-runner-controller");
-		return pipelineRunnerController(snap);
+		return pipelineRunnerController(change.after);
 	});
 
 exports.pipelineRunnerLarge = functions
 	.runWith(runnerSettings.large)
+	.region("asia-south1")
 	.firestore.document("ci-job-runs-queue/large")
-	.onCreate((snap) => {
+	.onWrite((change) => {
+		if (!change.after.exists) return;
 		const pipelineRunnerController = require("./api-controllers/pipeline-runner-controller");
-		return pipelineRunnerController(snap);
+		return pipelineRunnerController(change.after);
 	});
 
 // Write the required documents to trigger a pipeline runner from this webhook cloud function.
-exports.gitHubWebhook = functions.https.onRequest(async (req, res) => {
-	try {
-		const admin = require("./firebase/admin");
-		const batch = admin.firestore().batch();
-		const runId = admin.firestore().doc().id;
-		batch.set(admin.firestore().collection("runs").doc(runId), {
-			runId,
-			// TODO: Fill in the following bits of information from the correct sources
-			initialData: {
+exports.gitHubWebhook = functions
+	.region("asia-south1")
+	.https.onRequest(async (req, res) => {
+		try {
+			const admin = require("./firebase/admin");
+			const batch = admin.firestore().batch();
+			// Generate a unique ID
+			const runId = admin.firestore().collection("runs").doc().id;
+
+			batch.set(admin.firestore().collection("runs").doc(runId), {
 				runId,
-				repositoryURI: "https://github.com/deve-sh/greenlock-trial.git",
-				steps: [
-					{
-						name: "Install dependencies",
-						condition: "context.event == 'push'",
-						run: ['echo "Starting installation"', "npm install"],
-					},
-				],
-				projectId: "",
-				env: { ENABLE_DEPLOYS: "true" },
-				context: { event: "push", branchName: "main" },
-			},
-		});
-		batch.set(
-			admin.firestore().collection("ci-job-runs-queue").doc("standard"),
-			{ runId }
-		);
-		await batch.commit();
-		return res.send({ success: true });
-	} catch {
-		return res.send({ success: false });
-	}
-});
+				// TODO: Fill in the following bits of information from the correct sources
+				contextData: {
+					runId,
+					repositoryURI: "https://github.com/deve-sh/greenlock-trial.git",
+					steps: [
+						{
+							name: "Install dependencies",
+							condition: "context.event == 'push'",
+							run: ['echo "Starting installation"', "npm install"],
+						},
+					],
+					projectId: "",
+					env: { ENABLE_DEPLOYS: "true" },
+					context: { event: "push", branchName: "main" },
+				},
+			});
+			batch.set(
+				admin.firestore().collection("ci-job-runs-queue").doc("standard"),
+				{ runId }
+			);
+			await batch.commit();
+			return res.send({ success: true });
+		} catch (error) {
+			console.error(error);
+			return res.send({ success: false });
+		}
+	});
