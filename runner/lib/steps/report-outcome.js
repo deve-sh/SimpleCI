c7848d3f46187130e7899d1a44979129c07c7e36
Devesh Kumar <devesh2027@gmail.com>
2023-10-30 23:52:30 +0530

    runner: Update outcome reporter function

--- runner/lib/steps/report-outcome.js
+++ runner/lib/steps/report-outcome.js
@@ -1,10 +1,33 @@
+// @ts-check
+
 const runInfo = require("../run-info");
 
-const reportOutcome = async () => {
+const reportOutcome = async (
+	/**
+	 * @type { string }
+	 */
+	runId
+) => {
 	// Use runInfo and store the output to a database
-	// Logs to a log file
+	// and logs to a file on storage
+	// or everything to a database/storage
 
 	console.log(runInfo.stepsOutcome);
+
+	// For now just storing the information in a Database
+	// Do remember that logfiles are supposed to be the ideal place for storing this information.
+	// Databases have limits on the size of their entries as well as cost more per bit stored and bandwidth consumed.
+
+	const admin = require("../../firebase/admin");
+	// Don't forget to disable indexing on the logsAndMetadata field as it can get a little heavy.
+	await admin
+		.firestore()
+		.collection("runs")
+		.doc(runId)
+		.set(
+			{ status: runInfo.status, logsAndMetadata: runInfo.stepsOutcome },
+			{ merge: true }
+		);
 };
 
 module.exports = reportOutcome;
