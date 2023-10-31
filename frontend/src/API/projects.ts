8a2cff93e8b2660ed8f45901e6111ec14e8cb3d3
Devesh Kumar <devesh2027@gmail.com>
2023-10-31 22:24:11 +0530

    feat: frontend: Project APIs with DB/Firestore integration

---
+++ frontend/src/API/projects.ts
@@ -0,0 +1,110 @@
+import {
+	writeBatch,
+	doc,
+	serverTimestamp,
+	collection,
+	getDocs,
+	where,
+	query,
+	orderBy,
+	limit,
+	type Timestamp,
+} from "firebase/firestore";
+import { v4 as uuid } from "uuid";
+
+import db from "../providers/database";
+import auth from "../providers/auth";
+
+const COLLECTION_NAME = "simpleci-projects";
+
+export interface Project {
+	name: string;
+	repositoryId: string | number;
+	cloneURL: string;
+	url: string;
+	gitProvider: "github";
+	token: string;
+	ownedBy: string;
+	members: string[];
+	createdBy: string;
+	organization: string | null;
+	updatedBy: string;
+	createdAt: Timestamp;
+	updatedAt: Timestamp;
+	config: {
+		hookEvents: string[];
+		runnerPreference: "standard" | "medium" | "large";
+	};
+}
+
+export const createProject = async (project: Partial<Project>) => {
+	try {
+		if (!auth.currentUser) throw new Error("User isn't logged in.");
+
+		const batch = writeBatch(db);
+
+		const projectId = uuid();
+
+		const projectDoc = doc(db, COLLECTION_NAME, projectId);
+
+		batch.set(
+			projectDoc,
+			{
+				...project,
+				createdAt: serverTimestamp(),
+				updatedAt: serverTimestamp(),
+				createdBy: auth.currentUser.uid,
+				ownedBy: auth.currentUser.uid,
+				members: [auth.currentUser.uid],
+				updatedBy: auth.currentUser.uid,
+			},
+			{ merge: true }
+		);
+
+		await batch.commit();
+
+		return { error: null, project };
+	} catch (error) {
+		return { error };
+	}
+};
+
+export const getUserProjects = async () => {
+	try {
+		if (!auth.currentUser) throw new Error("User isn't logged in.");
+
+		const projectsQuery = query(
+			collection(db, COLLECTION_NAME),
+			where("members", "array-contains", auth.currentUser?.uid as string),
+			orderBy("createdAt", "desc")
+		);
+		const projects = (await getDocs(projectsQuery)).docs.map((project) => ({
+			...project,
+			createdAt: project.get("createdAt").toDate(),
+			updatedAt: project.get("updatedAt").toDate(),
+		}));
+
+		return { error: null, data: projects };
+	} catch (error) {
+		return { error, data: [] };
+	}
+};
+
+export const getUserProjectByRepoId = async (repoId: string | number) => {
+	try {
+		const projectQuery = query(
+			collection(db, COLLECTION_NAME),
+			where("members", "array-contains", auth.currentUser?.uid as string),
+			where("repositoryId", "==", repoId),
+			limit(1)
+		);
+		const projects = (await getDocs(projectQuery)).docs.map((project) => ({
+			...project,
+			createdAt: project.get("createdAt").toDate(),
+			updatedAt: project.get("updatedAt").toDate(),
+		}));
+		return { error: null, data: projects[0] };
+	} catch (error) {
+		return { error, data: null };
+	}
+};
